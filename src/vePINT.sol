b2f9c27d2249be60bb45402797946e92b27a398f
pintswap <dev@pintswap.exchange>
2023-08-13 21:14:33 +0000

    update

--- src/vePINT.sol
+++ src/vePINT.sol
@@ -6,8 +6,15 @@ import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IERC20Upgradeable} from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
 
 contract vePINT is ERC4626Upgradeable {
+    error NonTransferable();
     function initialize(IERC20Upgradeable underlying) public {
         __ERC20_init_unchained("vePINT", "vePINT");
         __ERC4626_init(underlying);
+    }
+    function transfer(address /* _to */, uint256 /* _amount */) public pure override(ERC20Upgradeable, IERC20Upgradeable) returns (bool) {
+      revert NonTransferable();
+    }
+    function transferFrom(address /* _from */, address /* _to */, uint256 /* amount */) public pure override(ERC20Upgradeable, IERC20Upgradeable)  returns (bool) {
+      revert NonTransferable();
     }
 }
