906aa03453c2577a3604f403c7c89ce1733e32a9
pintswap <dev@pintswap.exchange>
2023-08-13 11:32:33 -0500

    update

---
+++ src/PINT.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { ERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+
+contract PINT is ERC20Upgradeable {}
+  
