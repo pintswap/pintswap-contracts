9c89b413a99c591364e96b95b735d5d8aab5a1f9
pintswap <dev@pintswap.exchange>
2023-08-22 18:55:59 +0000

    update

--- src/test/PintDeployer.test.sol
+++ src/test/PintDeployer.test.sol
@@ -5,13 +5,14 @@ pragma solidity >=0.8.7 <0.9.0;
 import {Common} from "./Common.test.sol";
 import {PINTDeploy} from "../PINTDeployer.sol";
 import {PINT} from "../PINT.sol";
-import {vePINT} from "../vePINT.sol";
+import {sipERC20} from "../sipERC20.sol";
 import {console2} from "forge-std/console2.sol";
 import {ComputeCreateAddress} from "../utils/ComputeCreateAddress.sol";
+import {IERC20Metadata} from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract PintDeployerTest is Common {
     PINT pint;
-    vePINT vePint;
+    sipERC20 vePint;
     PINTDeploy pintDeploy;
 
     function setUp() public {
@@ -20,12 +21,10 @@ contract PintDeployerTest is Common {
         pintDeploy = new PINTDeploy();
         pint = PINT(
             payable(
-                ComputeCreateAddress.getCreateAddress(address(pintDeploy), 5)
+                ComputeCreateAddress.getCreateAddress(address(pintDeploy), 4)
             )
         );
-        vePint = vePINT(
-            (ComputeCreateAddress.getCreateAddress(address(pintDeploy), 3))
-        );
+        vePint = sipERC20(PINT(pint).ve());
     }
 
     function testBasicRun() public {
@@ -33,7 +32,16 @@ contract PintDeployerTest is Common {
         console2.log(address(100));
         assertEq(pint.name(), "PINT");
         assertEq(pint.symbol(), "PINT");
-        assertEq(vePint.name(), "vePINT");
-        assertEq(vePint.symbol(), "vePINT");
+        bytes32 hash;
+        address ve = address(vePint);
+        assembly {
+          hash := extcodehash(ve)
+        }
+        console2.log(uint256(hash));
+/*
+        console2.log(vePint.name());
+        assertEq(bytes(vePint.symbol()), bytes("sipPINT"));
+        assertEq(vePint.symbol(), "sipPINT");
+*/
     }
 }
