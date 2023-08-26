13991bcc8873a2869046e44b73c0049a84d09e7f
v16s <pintswap@pintswap>
2023-08-26 21:45:02 +0530

    add tests

--- src/test/Common.test.sol
+++ src/test/Common.test.sol
@@ -6,6 +6,7 @@ import {PINT} from "../PINT.sol";
 import {sipERC20} from "../sipERC20.sol";
 import {PINTDeploy} from "../PINTDeployer.sol";
 import {ComputeCreateAddress} from "../utils/ComputeCreateAddress.sol";
+import {OPPS} from "../OPPS.sol";
 import {IERC20Metadata} from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract Common is Test {
@@ -14,13 +15,20 @@ contract Common is Test {
     PINT pint;
     sipERC20 vePint;
     PINTDeploy pintDeploy;
+    OPPS opps;
+
+    address constant treasury =
+        address(0xEC3de41D5eAD4cebFfD656f7FC9d1a8d8Ff0f8c0);
 
     function setUpBase() public {
-        pintDeploy = new PINTDeploy();
+        pintDeploy = new PINTDeploy(treasury);
         pint = PINT(
             payable(
                 ComputeCreateAddress.getCreateAddress(address(pintDeploy), 4)
             )
+        );
+        opps = OPPS(
+            ComputeCreateAddress.getCreateAddress(address(pintDeploy), 2)
         );
         vePint = sipERC20(PINT(pint).ve());
     }
--- src/test/Reverts.test.sol
+++ src/test/Reverts.test.sol
@@ -8,10 +8,36 @@ contract Reverts is Common {
     function setUp() public {
         initializeMainnetFork();
         setUpBase();
+        //send pint to address(100)
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        pint.enableTrading();
+        vm.startPrank(treasury);
+        pint.transfer(address(100), 1 ether);
+        vm.stopPrank();
+        vm.startPrank(address(100));
+        vm.expectRevert(bytes("Not authorized to transfer pre-migration."));
+        pint.transfer(address(200), 1 ether);
+        vm.stopPrank();
+        vm.startPrank(treasury);
+        pint.enableTrading();
+        opps.mint(address(100), 0);
+        vm.stopPrank();
+        vm.startPrank(address(100));
+        pint.approve(address(vePint), ~uint256(1));
+        vm.stopPrank();
     }
 
     function testRevertOnWrongBorrow() public {
+        vm.expectRevert(bytes("ERC721Enumerable: owner index out of bounds"));
         vePint.finna(1 ether);
-        vm.expectRevert(bytes(""));
+        vm.expectRevert(bytes("ERC721Enumerable: owner index out of bounds"));
+        vePint.push(1 ether, 0);
+
+        vm.startPrank(address(100));
+        vePint.deposit(1 ether, address(100));
+
+        vePint.finna(1 ether);
+        vm.expectRevert(bytes("ERC20: transfer amount exceeds balance"));
+        vePint.finna(1 ether);
     }
 }
