8929bc8502fec016981dbe3f5752d32ea8366181
UndercovercameL <undercover@camel>
2023-08-28 09:47:46 +0000

    add test

--- src/test/PINT.test.sol
+++ src/test/PINT.test.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.8.7 <0.9.0;
 
 import {Common} from "./Common.test.sol";
+import "forge-std/console2.sol";
 
 contract PintTest is Common {
     function setUp() public {
@@ -10,7 +11,21 @@ contract PintTest is Common {
         setUpBase();
     }
 
-    function testPintLimits() public {}
+    function testPintLimits() public {
+        vm.startPrank(treasury);
+        pint.enableTrading();
+        pint.transfer(address(100), pint.maxTransactionAmount());
+        vm.startPrank(address(100));
+        console2.log(pint.maxWallet() > pint.maxTransactionAmount());
+        vm.expectRevert(bytes("Max wallet exceeded"));
+        pint.transfer(address(200), pint.maxTransactionAmount() + 2);
+        vm.startPrank(treasury);
+        pint.transfer(address(100), 1 ether);
+        vm.startPrank(address(100));
+
+        // vm.expectRevert(bytes("Max wallet exceeded"));
+        pint.transfer(address(200), 1 ether);
+    }
 
     function testPintFees() public {}
 
