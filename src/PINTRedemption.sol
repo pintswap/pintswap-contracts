82d1f74c7a70f775c8f9939edaa4a5c2aef93e4e
pintswap <dev@pintswap.exchange>
2023-09-04 16:13:56 +0000

    update

--- src/PINTRedemption.sol
+++ src/PINTRedemption.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
-import { SafeERC20 } from "openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol";
-import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import { IERC721 } from "openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
+import { SafeERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { IERC20 as IERC20Sablier } from "v2-core/src/types/Tokens.sol";
+import { IERC721 } from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import { ISablierV2LockupLinear } from "v2-core/src/interfaces/ISablierV2LockupLinear.sol";
+import { Broker, LockupLinear } from "v2-core/src/types/DataTypes.sol";
+import { ud60x18 } from "v2-core/src/types/Math.sol";
+
 
 contract PINTRedemption {
   using SafeERC20 for IERC20;
@@ -12,20 +17,35 @@ contract PINTRedemption {
   address immutable pint;
   uint256 constant WOCK_REDEMPTION = 1e24;
   uint256 constant TRIS_REDEMPTION = 1e23;
+  ISablierV2LockupLinear constant lockupLinear = ISablierV2LockupLinear(0xB10daee1FCF62243aE27776D7a92D39dC8740f95);
   constructor(address _pint) {
     pint = _pint;
   }
-  function redeemWOCK(uint256[] memory tokenIds) public returns (uint256 output) {
+  function redeemWOCK(uint256[] memory tokenIds) public returns (uint256 streamId) {
     for (uint256 i = 0; i < tokenIds.length; i++) {
       IERC721(wock).safeTransferFrom(msg.sender, address(0x0), tokenIds[i]);
     }
-    IERC20(pint).safeTransferFrom(treasury, msg.sender, tokenIds.length*WOCK_REDEMPTION);
+    uint256 total = tokenIds.length*WOCK_REDEMPTION;
+    IERC20(pint).safeTransferFrom(treasury, msg.sender, total/4);
+    LockupLinear.CreateWithDurations memory params;
+    params.sender = treasury;
+    params.recipient = msg.sender;
+    params.totalAmount = uint128(total*3/4);
+    params.asset = IERC20Sablier(pint);
+    params.cancelable = true;
+    params.durations = LockupLinear.Durations({
+      cliff: 0 weeks,
+      total: 52 weeks
+    });
+    params.broker = Broker(address(0x0), ud60x18(0));
+    streamId = lockupLinear.createWithDurations(params);
   }
   function redeemTRIS(uint256[] memory tokenIds) public returns (uint256 output) {
     for (uint256 i = 0; i < tokenIds.length; i++) {
       IERC721(wock).safeTransferFrom(msg.sender, address(0x0), tokenIds[i]);
     }
-    IERC20(pint).safeTransferFrom(treasury, msg.sender, tokenIds.length*TRIS_REDEMPTION);
+    output = tokenIds.length*TRIS_REDEMPTION;
+    IERC20(pint).safeTransferFrom(treasury, msg.sender, output);
   }
 }
   
