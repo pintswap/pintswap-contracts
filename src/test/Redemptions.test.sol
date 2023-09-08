be43673d6d7f883652afbfab328879c6c927912d
v16s <pintswap@pintswap>
2023-09-08 20:33:58 +0530

    changes to sablier stream test

--- src/test/Redemptions.test.sol
+++ src/test/Redemptions.test.sol
@@ -4,10 +4,14 @@ pragma solidity >=0.8.7 <0.9.0;
 
 import {Common} from "./Common.test.sol";
 import {Math} from "openzeppelin-contracts/contracts/utils/math/Math.sol";
+import "forge-std/console2.sol";
+import {ISablierV2LockupLinear} from "v2-core/src/interfaces/ISablierV2LockupLinear.sol";
 
 contract RedemptionTest is Common {
     uint[] tokenIds;
 
+    ISablierV2LockupLinear constant lockupLinear =
+        ISablierV2LockupLinear(0xB10daee1FCF62243aE27776D7a92D39dC8740f95);
     uint256 constant REDEMPTION = 1e24;
 
     function setUp() public {
@@ -23,21 +27,21 @@ contract RedemptionTest is Common {
                     keccak256(
                         abi.encodePacked(
                             address(tris),
-                            (i * 100),
-                            address(uint160((100 + (i * 100))))
+                            (i * 1000),
+                            address(uint160((1000 + (i * 1000))))
                         )
                     )
                 )
             );
             vm.prank(tris.owner());
-            tris.adminMint(address(uint160((100 + (i * 100)))), tokenIds[i]);
+            tris.adminMint(address(uint160((1000 + (i * 1000)))), tokenIds[i]);
             vm.prank(wock.owner());
-            wock.mint(address(uint160((100 + (i * 100)))), tokenIds[i]);
+            wock.mint(address(uint160((1000 + (i * 1000)))), tokenIds[i]);
         }
     }
 
     function testTRISRedemption() public {
-        vm.startPrank(address(100));
+        vm.startPrank(address(1000));
         uint[] memory _tokenIds = new uint[](1);
         _tokenIds[0] = tokenIds[0];
         tris.setApprovalForAll(address(trisRedemption), true);
@@ -45,18 +49,19 @@ contract RedemptionTest is Common {
     }
 
     function testSablierStream() public {
-        vm.startPrank(address(100));
+        vm.startPrank(address(1000));
         uint[] memory _tokenIds = new uint[](1);
         _tokenIds[0] = tokenIds[0];
         wock.setApprovalForAll(address(wockRedemption), true);
         wockRedemption.redeem(_tokenIds);
-        vm.startPrank(address(200));
+        vm.startPrank(address(2000));
         _tokenIds[0] = tokenIds[1];
         wock.setApprovalForAll(address(wockRedemption), true);
         uint streamid = wockRedemption.redeem(_tokenIds);
+        uint time = block.timestamp;
 
-        assertEq(streamid, 0);
-        uint256 vested = Math.max(
+        require(streamid != 0, "wrong streamId");
+        uint256 vested = Math.min(
             REDEMPTION,
             REDEMPTION /
                 4 +
@@ -65,6 +70,20 @@ contract RedemptionTest is Common {
                     REDEMPTION) /
                 (52 weeks)
         );
+
+        uint256 total = REDEMPTION;
+        assertEq(pint.balanceOf(address(1000)), vested);
         vm.warp(block.timestamp + 10 weeks);
+
+        uint expectedAmount = ((REDEMPTION - vested) *
+            (block.timestamp - time)) /
+            (52 weeks - (time - wockRedemption.startTime()));
+        console2.log(expectedAmount);
+
+        lockupLinear.withdrawMax(streamid, address(2000));
+        uint currentAmount = pint.balanceOf(address(2000)) - vested;
+        console2.log(currentAmount);
+        uint diff = uint(int(expectedAmount - currentAmount));
+        console2.log(diff);
     }
 }
