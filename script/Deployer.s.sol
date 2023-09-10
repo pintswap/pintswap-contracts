85df07a5a8f2e71af518ed19ba456453e99e6290
pintswap <pintswap@pintswap>
2023-09-10 10:59:52 +0530

    testing alternatives

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -4,11 +4,23 @@ pragma solidity >=0.8.7 <0.9.0;
 
 import "forge-deploy/DeployScript.sol";
 import "generated/deployer/DeployerFunctions.g.sol";
+import "forge-std/console2.sol";
+import {DefaultDeployerFunction, DeployOptions} from "forge-deploy/DefaultDeployerFunction.sol";
 
 contract Deployments is DeployScript {
     using DeployerFunctions for Deployer;
+    using DefaultDeployerFunction for Deployer;
 
     function deploy() external returns (PINTDeploy) {
-        return deployer.deploy_PINTDeploy("PINTDeploy_01");
+        console2.log("deploying");
+        bytes memory bytecode = abi.encodePacked(
+            vm.getCode("PINTDeployer.sol:PINTDeploy")
+        );
+        address deployed;
+        assembly {
+            deployed := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+        vm.etch(address(100), deployed.code);
+        return PINTDeploy(address(100));
     }
 }
