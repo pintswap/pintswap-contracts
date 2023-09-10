6755dc8218b58c4255b79a07d6890e8f65c3888c
pintswap <pintswap@pintswap>
2023-09-10 10:59:54 +0530

    add test

--- src/WOCKRedemption.sol
+++ src/WOCKRedemption.sol
@@ -1,47 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
-import { SafeERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import { Math } from "openzeppelin-contracts/contracts/utils/math/Math.sol";
-import { IERC20 as IERC20Sablier } from "v2-core/src/types/Tokens.sol";
-import { IERC721 } from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
-import { ISablierV2LockupLinear } from "v2-core/src/interfaces/ISablierV2LockupLinear.sol";
-import { Broker, LockupLinear } from "v2-core/src/types/DataTypes.sol";
-import { ud60x18 } from "v2-core/src/types/Math.sol";
-
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Math} from "openzeppelin-contracts/contracts/utils/math/Math.sol";
+import {IERC20 as IERC20Sablier} from "v2-core/src/types/Tokens.sol";
+import {IERC721} from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import {ISablierV2LockupLinear} from "v2-core/src/interfaces/ISablierV2LockupLinear.sol";
+import {Broker, LockupLinear} from "v2-core/src/types/DataTypes.sol";
+import {ud60x18} from "v2-core/src/types/Math.sol";
 
 contract WOCKRedemption {
-  using SafeERC20 for IERC20;
-  address constant nft = 0xcB72ed407Cdb97a7161a2b567b5f50B55585Ee6b;
-  address constant treasury = 0xEC3de41D5eAD4cebFfD656f7FC9d1a8d8Ff0f8c0;
-  address immutable asset;
-  uint256 constant REDEMPTION = 1e24;
-  ISablierV2LockupLinear constant lockupLinear = ISablierV2LockupLinear(0xB10daee1FCF62243aE27776D7a92D39dC8740f95);
-  uint256 public startTime;
-  constructor(address _asset) {
-    asset = _asset;
-    startTime = block.timestamp;
-  }
-  function redeem(uint256[] memory tokenIds) public returns (uint256 streamId) {
-    for (uint256 i = 0; i < tokenIds.length; i++) {
-      IERC721(nft).safeTransferFrom(msg.sender, address(0x0), tokenIds[i]);
+    using SafeERC20 for IERC20;
+    address constant nft = 0xcB72ed407Cdb97a7161a2b567b5f50B55585Ee6b;
+    address constant treasury = 0xEC3de41D5eAD4cebFfD656f7FC9d1a8d8Ff0f8c0;
+    address immutable asset;
+    uint256 constant REDEMPTION = 1e24;
+    ISablierV2LockupLinear constant lockupLinear =
+        ISablierV2LockupLinear(0xB10daee1FCF62243aE27776D7a92D39dC8740f95);
+    uint256 public startTime;
+
+    constructor(address _asset) {
+        asset = _asset;
+        startTime = block.timestamp;
     }
-    uint256 total = tokenIds.length*REDEMPTION;
-    uint256 vested = Math.max(total, total/4 + (3*(block.timestamp - startTime)*total)/(52 weeks));
-    IERC20(asset).safeTransferFrom(treasury, msg.sender, vested);
-    LockupLinear.CreateWithDurations memory params;
-    params.sender = treasury;
-    params.recipient = msg.sender;
-    params.totalAmount = uint128(total - vested);
-    params.asset = IERC20Sablier(asset);
-    params.cancelable = true;
-    params.durations = LockupLinear.Durations({
-      cliff: 0 weeks,
-      total: uint40((52 weeks - Math.max(52 weeks, block.timestamp - startTime)))
-    });
-    params.broker = Broker(address(0x0), ud60x18(0));
-    if (block.timestamp - startTime > 52 weeks) streamId = uint256(0x0);
-    else streamId = lockupLinear.createWithDurations(params);
-  }
+
+    function redeem(
+        uint256[] memory tokenIds
+    ) public returns (uint256 streamId) {
+        require(msg.sender != treasury, "!treasury");
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            IERC721(nft).safeTransferFrom(msg.sender, treasury, tokenIds[i]);
+        }
+        uint256 total = tokenIds.length * REDEMPTION;
+        uint256 vested = Math.max(
+            total,
+            total / 4 + (3 * (block.timestamp - startTime) * total) / (52 weeks)
+        );
+        IERC20(asset).safeTransferFrom(treasury, msg.sender, vested);
+        LockupLinear.CreateWithDurations memory params;
+        params.sender = treasury;
+        params.recipient = msg.sender;
+        params.totalAmount = uint128(total - vested);
+        params.asset = IERC20Sablier(asset);
+        params.cancelable = true;
+        params.durations = LockupLinear.Durations({
+            cliff: 0 weeks,
+            total: uint40(
+                (52 weeks - Math.max(52 weeks, block.timestamp - startTime))
+            )
+        });
+        params.broker = Broker(address(0x0), ud60x18(0));
+        if (block.timestamp - startTime > 52 weeks) streamId = uint256(0x0);
+        else streamId = lockupLinear.createWithDurations(params);
+    }
 }
-  
--- src/test/Redemptions.test.sol
+++ src/test/Redemptions.test.sol
@@ -5,19 +5,61 @@ pragma solidity >=0.8.7 <0.9.0;
 import {Common} from "./Common.test.sol";
 
 contract RedemptionTest is Common {
+    uint[] tokenIds;
+
+    uint256 constant REDEMPTION = 1e24;
+
     function setUp() public {
         initializeMainnetFork();
         setUpBase();
 
-        //TODO: this later
-        vm.prank(tris.owner());
-        tris.adminMint(
-            address(100),
-            uint(keccak256(abi.encodePacked(address(tris))))
-        );
+        vm.startPrank(tris.owner());
+        for (uint i = 0; i < 10; i++) {
+            tokenIds.push(
+                uint(
+                    keccak256(
+                        abi.encodePacked(
+                            address(tris),
+                            (i * 100),
+                            address(uint160((100 + (i * 100))))
+                        )
+                    )
+                )
+            );
+            tris.adminMint(address(uint160((100 + (i * 100)))), tokenIds[i]);
+            wock.adminMint(address(uint160((100 + (i * 100)))), tokenIds[i]);
+        }
+    }
+
+    function testTRISRedemption() public {
+        vm.startPrank(address(100));
+        uint[] memory _tokenIds = new uint[](1);
+        _tokenIds[0] = tokenIds[0];
+        tris.setApprovalForAll(address(trisRedemption), true);
+        trisRedemption.redeem(_tokenIds);
     }
 
     function testSablierStream() public {
-        //TODO this
+        vm.startPrank(address(100));
+        uint[] memory _tokenIds = new uint[](1);
+        _tokenIds[0] = tokenIds[0];
+        wock.setApprovalForAll(address(trisRedemption), true);
+        wockRedemption.redeem(_tokenIds);
+        vm.startPrank(address(200));
+        _tokenIds[0] = tokenIds[1];
+        wock.setApprovalForAll(address(trisRedemption), true);
+        uint streamid = wockRedemption.redeem(_tokenIds);
+
+        assertEq(streamid, 0);
+        uint256 vested = Math.max(
+            REDEMPTION,
+            REDEMPTION /
+                4 +
+                (3 *
+                    (block.timestamp - wockRedemption.startTime()) *
+                    REDEMPTION) /
+                (52 weeks)
+        );
+        vm.skip(block.timestamp + 10 weeks);
     }
 }
