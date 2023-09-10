144fbe9c60ed21967c6aa6d3da988feafd5ed489
pintswap <pintswap@pintswap>
2023-09-10 10:59:54 +0530

    add uniswap tests

--- src/test/PINT.test.sol
+++ src/test/PINT.test.sol
@@ -38,6 +38,21 @@ contract PintTest is Common {
             (1 ether) - (((1 ether) * 5) / (100))
         );
         assertEq(pint.balanceOf(address(pint)), ((1 ether) * 5) / 100);
+        vm.startPrank(treasury);
+        pint.transfer(address(100), 1 ether - pint.balanceOf(address(100)));
+        vm.startPrank(address(100));
+        address[] memory path = new address[](2);
+        path[0] = address(pint);
+        path[1] = address(weth);
+        console2.log("swapping");
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            1 ether,
+            1,
+            path,
+            address(100),
+            block.timestamp + 200
+        );
+        console2.log(pint.balanceOf(address(vePint)));
     }
 
     function testBlacklist() public {
