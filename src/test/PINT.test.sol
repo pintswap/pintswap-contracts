8b29dc84666dfe08ce0930c9ee8dd50bf206d674
pintswap <pintswap@pintswap>
2023-09-10 10:59:54 +0530

    add more tests

--- src/test/PINT.test.sol
+++ src/test/PINT.test.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.7 <0.9.0;
 
 import {Common} from "./Common.test.sol";
 import "forge-std/console2.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract PintTest is Common {
     function setUp() public {
@@ -39,20 +40,33 @@ contract PintTest is Common {
         );
         assertEq(pint.balanceOf(address(pint)), ((1 ether) * 5) / 100);
         vm.startPrank(treasury);
-        pint.transfer(address(100), 1 ether - pint.balanceOf(address(100)));
+        pint.transfer(
+            address(100),
+            1 ether - pint.balanceOf(address(100)) + 10000000 ether
+        );
         vm.startPrank(address(100));
         address[] memory path = new address[](2);
         path[0] = address(pint);
         path[1] = address(weth);
-        console2.log("swapping");
+        uint balanceBefore = pint.balanceOf(address(pint));
         router.swapExactTokensForETHSupportingFeeOnTransferTokens(
-            1 ether,
+            1000000 ether,
             1,
             path,
             address(100),
             block.timestamp + 200
         );
-        console2.log(pint.balanceOf(address(vePint)));
+        require(
+            pint.balanceOf(address(pint)) - balanceBefore > 0,
+            "fees not deducted"
+        );
+        pint.transfer(
+            address(pint),
+            pint.swapTokensAtAmount() - pint.balanceOf(address(pint))
+        );
+        uint treasuryBalanceBefore = treasury.balance;
+        pint.transfer(address(300), 1 ether);
+        require(treasury.balance - treasuryBalanceBefore >= 0, "!swap");
     }
 
     function testBlacklist() public {
