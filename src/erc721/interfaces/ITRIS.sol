e849ce0c222ef675fa548fd13a2e039a8b56c8ad
pintswap <pintswap@pintswap>
2023-09-10 10:59:54 +0530

    add itris

---
+++ src/erc721/interfaces/ITRIS.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.7 <0.9.0;
+
+import {IERC721} from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+
+interface ITRIS is IERC721 {
+    function owner() external returns (address);
+
+    function adminMint(address, uint) external;
+}
